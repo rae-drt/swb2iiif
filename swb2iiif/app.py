abbdd30f15210b3c294ceee726d7d1271ed4117a
John Moore <john.moore@nationalarchives.gov.uk>
2023-10-25 15:50:05 +0100

    make more commandline friendly

--- swb2iiif/app.py
+++ swb2iiif/app.py
@@ -1,35 +1,44 @@
+import click
 import pandas as pd
+from configparser import ConfigParser
 from iiif_prezi3 import Manifest, Canvas, AnnotationPage, Annotation, ResourceItem, ResourceItem1
 
-def read_csv(file_name):
-    for row in pd.read_csv(file_name, iterator=True):
-        yield row
+config_ini = ConfigParser()
+config_ini.read("config.ini")
+
+@click.command()
+@click.option("--csv", required=True, help="csv file to parse.")
+@click.option("--domain", required=False, help="filter by domain.")
+@click.option("--search-service", required=False, show_default=True, default=config_ini.get("swb2iiif", "SEARCH_SERVICE"), help="content search service.")
+@click.option("--uri-prefix", required=False, default=config_ini.get("swb2iiif", "URI_PREFIX"), help="used to form URI id.")
+@click.option("--label", required=False, default=config_ini.get("swb2iiif", "LABEL"), help="manifest label.")
+@click.version_option(prog_name=config_ini.get("main", "NAME"), version=config_ini.get("main", "VERSION"))
 
 
-manifest = Manifest(id="https://example.com/iiif/manifest.json", label={"en":["Example export from Solrwayback"]}, behavior="paged")
+def run(csv, search_service, domain, uri_prefix, label):
+    manifest = Manifest(id=f"{uri_prefix}/manifest.json", label={"en":[label]}, behavior="paged")
+    manifest.make_service(id=search_service, type="SearchService2")
+    df = pd.read_csv(csv)
+    dedup_df = df.drop_duplicates(subset=['description'])
+    for row in dedup_df.itertuples(index=True, name='Pandas'):
+        url = getattr(row, "url")
+        id = getattr(row, "id")
+        domain = getattr(row, "domain")
+        description = getattr(row, "description")
+        if pd.notnull(description):
+            painting_resource_item = ResourceItem(id=url, type="Image", height="3024", width="4032")
+            annotation_page = AnnotationPage(id=f"{uri_prefix}/annotation-page/{id}")
+            painting_item = Annotation(id=f"{uri_prefix}/painting/{id}", body=painting_resource_item, target=f"{uri_prefix}/canvas-{id}", motivation="painting")
+            annotation_page.add_item(painting_item)
+            canvas = Canvas(id=f"{uri_prefix}/canvas/{id}", height="3024", width="4032")
+            canvas.add_item(annotation_page)
+            manifest.add_item(canvas)
+            commenting_resource_item = ResourceItem1(id=f"{uri_prefix}/text/{id}", type="TextualBody", value=description)
+            commenting_item = Annotation(id=f"{uri_prefix}/comment/{id}", body=commenting_resource_item, target=f"{uri_prefix}/canvas-{id}", motivation="commenting")
+            canvas.add_annotation(commenting_item)
+    print(manifest.json(indent=2))
 
-manifest.make_service(id="http://pi.zedstar.org:5555/search", type="SearchService2")
 
-
-df = pd.read_csv('../export/solrwayback_2023-10-18_15-30-22.csv')
-dedup_df = df.drop_duplicates(subset=['description'])
-for row in dedup_df.itertuples(index=True, name='Pandas'):
-    url = getattr(row, "url")
-    id = getattr(row, "id")
-    description = getattr(row, "description")
-    if pd.notnull(description):
-        resource_item = ResourceItem(id=url, type="Image", height="3024", width="4032")
-        annotation_page = AnnotationPage(id=f"https://example.com/iiif/annopage-1/{id}")
-        item = Annotation(id=f"https://example.com/iiif/anno-1/{id}", body=resource_item, target=f"https://example.com/iiif/canvas-{id}", motivation="painting")
-        annotation_page.add_item(item)
-        canvas = Canvas(id=f"https://example.com/iiif/canvas-{id}", height="3024", width="4032")
-        canvas.add_item(annotation_page)
-        manifest.add_item(canvas)
-        anno_resource_item = ResourceItem1(id=f"https://example.com/iiif/annopage-2/anno-1/{id}", type="TextualBody", value=description)
-        anno_item = Annotation(id=f"https://example.com/iiif/anno-2/{id}", body=anno_resource_item, target=f"https://example.com/iiif/canvas-{id}", motivation="commenting")
-        canvas.add_annotation(anno_item)
-    
-
-print(manifest.json(indent=2))
-
+if __name__ == "__main__":
+    run()
 
